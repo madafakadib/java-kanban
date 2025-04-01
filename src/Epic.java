057bb4de324169fa5a0746013b143c7f231b02f2
Denis <dessitnov22@yandex.ru>
2025-04-01 09:32:27 +0300

    Task completed

--- src/Epic.java
+++
@@ -1,9 +0,0 @@
-public class Epic extends Task{
-    public Epic(String name, String description) {
-        super(name, description);
-    }
-
-    public Epic(String name, String description, Status status) {
-        super(name, description, status);
-    }
-}
--- src/Subtask.java
+++
@@ -1,9 +0,0 @@
-public class Subtask extends Task{
-    public Subtask(String name, String description) {
-        super(name, description);
-    }
-
-    public Subtask(String name, String description, Status status) {
-        super(name, description, status);
-    }
-}
